4122e0ff23435dd4813e13470beec85e170451f6
Richardson Schawarski Cruz <125823124+professorrichardson@users.noreply.github.com>
2025-03-04 22:23:47 -0300

    Fix Button

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
 
     let tamanhoAtualFonte = 1;
 
